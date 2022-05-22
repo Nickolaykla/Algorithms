f819faf41c42bb7bb86257f163308224d11ea984
Nickolay Levotsky <nickolaykla@gmail.com>
2022-05-22 17:51:02 +0300

    memoization fib added

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Fibonacci {
     public static void main(String[] args) {
         System.out.println(fib(50));
         System.out.println(fibRec(10));
+
+        int n = 100;
+        long[] memo = new long[n + 1];
+        Arrays.fill(memo, -1);
+        System.out.println(fibMemo((n),memo));
     }
     // O(n)
     public static long fib(int n) {
@@ -21,5 +27,13 @@ public class Fibonacci {
         if (n <= 1) return n;
         return fibRec(n - 1) + fibRec(n - 2);
     }
+    // memoization O(n)
+    public static long fibMemo(int n, long[] memo) {
+        if (memo[n] != -1) return memo[n];
+        if (n <= 1) return n;
+        long result = fibMemo((n - 1), memo) + fibMemo((n - 2), memo);
 
+        memo[n] = result;
+        return result;
+    }
 }
